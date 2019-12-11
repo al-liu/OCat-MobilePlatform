6f9a1a7035d6c0610c3d82577b215325135ccc51
刘海川 <lhc.indie@gmail.com>
2019-12-11 17:54:15 +0800

    Update AbstractPublishPackage.java
    不依赖上传包的命名（原来需要用版本号命名），如果不是版本号命名包，会用 versionName 进行重命名。

--- mobileplatform/src/main/java/com/lhc/ocat/mobileplatform/publishpackage/AbstractPublishPackage.java
+++ mobileplatform/src/main/java/com/lhc/ocat/mobileplatform/publishpackage/AbstractPublishPackage.java
@@ -4,6 +4,10 @@ import com.lhc.ocat.mobileplatform.domain.dos.ApplicationDO;
 import com.lhc.ocat.mobileplatform.domain.dos.ResourceDO;
 import com.lhc.ocat.mobileplatform.domain.dto.Patch;
 import com.lhc.ocat.mobileplatform.domain.dto.Resource;
+import com.lhc.ocat.mobileplatform.exception.ApiErrorType;
+import com.lhc.ocat.mobileplatform.exception.ApiException;
+import com.lhc.ocat.mobileplatform.exception.SystemErrorType;
+import lombok.extern.log4j.Log4j2;
 import net.lingala.zip4j.ZipFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,16 +16,19 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
  * @author lhc
  * @date 2019-11-01 15:26
  */
+@Log4j2
 public abstract class AbstractPublishPackage implements PublishPackage {
 
     private String workshopPath;
     private String maxDiffCount;
+    private static final int WORKSHOP_INIT_FILE_COUNT = 2;
 
     AbstractPublishPackage(String workshopPath, String maxDiffCount) {
         this.workshopPath = workshopPath;
@@ -32,11 +39,57 @@ public abstract class AbstractPublishPackage implements PublishPackage {
     public void createPatchPackage(MultipartFile uploadPackage,
                                    String versionName,
                                    int versionCode,
-                                   ApplicationDO applicationDO)  throws IOException {
+                                   ApplicationDO applicationDO)  throws Exception {
+        // 如果 workshop 目录存在则先删除
+        File workshopFile = new File(workshopPath);
+        if (workshopFile.exists()) {
+            DiffPackageUtil.cleanFiles(workshopFile);
+        }
         // 将上传文件移动到指定目录 workshop 并解压包。
         String allZipPackagePath = Paths.get(workshopPath, ALL_PACKAGE_NAME).toString();
         File allZipPackageFile = new File(allZipPackagePath);
         DiffPackageUtil.unzipUploadPackage(uploadPackage, allZipPackageFile);
+        // 将解压后的包文件命名成 versionName
+        if (workshopFile.exists()) {
+            File[] files = workshopFile.listFiles();
+            if (Objects.nonNull(files)) {
+                String uploadPackageName = "";
+                // 找到解压后的文件夹名
+                if (files.length == WORKSHOP_INIT_FILE_COUNT) {
+                    for (File file :
+                            files) {
+                        if (!file.getName().contains(ZIP_FILE_SUFFIX)) {
+                            uploadPackageName = file.getName();
+                        }
+                    }
+                } else {
+                    // 应该只有两个文件
+                    log.error("workshop 初始解压后的文件数不为 2");
+                    throw new ApiException(SystemErrorType.SYSTEM_ERROR, "差量包工作异常");
+                }
+                if (!uploadPackageName.equals(versionName)) {
+                    log.debug("开始改名包文件");
+                    String uploadFilePath = Paths.get(workshopPath, uploadPackageName).toString();
+                    String renameFilePath = Paths.get(workshopPath, versionName).toString();
+                    File uploadFile = new File(uploadFilePath);
+                    File renameFile = new File(renameFilePath);
+                    boolean renameResult = uploadFile.renameTo(renameFile);
+                    if (!renameResult) {
+                        log.error("差量包重命名初始的解压包失败");
+                    }
+                    // 删除旧 all.zip 包，压缩新 all.zip 包
+                    boolean deleteResult = allZipPackageFile.delete();
+                    if (!deleteResult) {
+                        log.error("差量包删除初始的 all.zip 解压包失败");
+                    }
+                    new ZipFile(allZipPackageFile).addFolder(renameFile);
+                }// if end：如果解压后的文件夹名与上送版本号一样则不用做重命名和重打压缩包的操作
+            } else {
+                // 工作目录下为有响应文件
+                log.error("workshop 初始解压后的 file[] 为 null");
+                throw new ApiException(SystemErrorType.SYSTEM_ERROR, "差量包工作异常");
+            }
+        }
         // 解压后包目录
         String allUnzipPackagePath = Paths.get(workshopPath, versionName).toString();
         File allUnzipPackageFile = new File(allUnzipPackagePath);
