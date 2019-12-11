74a62d9141857c0b4c4ccfb8f18dfd4aa68ae3bc
刘海川 <lhc.indie@gmail.com>
2019-12-11 17:53:16 +0800

    Create UserLockParam.java

---
+++ mobileplatform/src/main/java/com/lhc/ocat/mobileplatform/domain/param/UserLockParam.java
@@ -0,0 +1,12 @@
+package com.lhc.ocat.mobileplatform.domain.param;
+
+import lombok.Data;
+
+/**
+ * @author lhc
+ * @date 2019-12-06 10:34
+ */
+@Data
+public class UserLockParam {
+    private Integer enabled;
+}
