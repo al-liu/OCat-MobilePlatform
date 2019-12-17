77e52edc69b8515dc8c10230af5964c4ddc29d37
刘海川 <lhc.indie@gmail.com>
2019-12-17 09:46:17 +0800

    修改 demo ui

--- android-demo/app/src/main/java/com/lhc/ocat/android_demo/MainActivity.java
+++ android-demo/app/src/main/java/com/lhc/ocat/android_demo/MainActivity.java
@@ -4,18 +4,17 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.lhc.ocat.android_demo.manager.PackageManager;
+import com.lhc.ocat.android_demo.manager.PackageSettings;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String PRE_PACKAGE_VERSION = "1.0.0";
 
-    private WebView mWebView;
-    private Button loadButton;
+    private TextView inbuiltTextView, currentTextView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -23,22 +22,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PackageManager
-                .sharedInstance()
-                .startUp(PRE_PACKAGE_VERSION, MainActivity.this);
+        PackageSettings packageSettings = new PackageSettings(
+                "8405288128",
+                "c9663f262eb24fe3a859f0113c98efca",
+                "http://localhost:8800",
+                PRE_PACKAGE_VERSION);
+        PackageManager.manageWithSettings(packageSettings).launch(MainActivity.this);
 
-        mWebView = findViewById(R.id.webView);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        loadButton = findViewById(R.id.loadTest);
-        loadButton.setOnClickListener(this);
+        inbuiltTextView = findViewById(R.id.inbuiltVersion);
+        currentTextView = findViewById(R.id.currentVersion);
+        Button seeOfflineButton = findViewById(R.id.seeOffline);
+        Button seeOnlineButton = findViewById(R.id.seeOnline);
+        Button updatePatchButton = findViewById(R.id.updatePatch);
+        Button clearCacheButton = findViewById(R.id.clearCache);
+        seeOfflineButton.setOnClickListener(this);
+        seeOnlineButton.setOnClickListener(this);
+        updatePatchButton.setOnClickListener(this);
+        clearCacheButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.loadTest:
-                mWebView.loadUrl("http://localhost:8887");
+            case R.id.seeOffline:
+                break;
+            case R.id.seeOnline:
+                break;
+            case R.id.updatePatch:
+                PackageManager.sharedInstance().updateLatestPatch(MainActivity.this);
+                break;
+            case R.id.clearCache:
                 break;
         }
     }
---
+++ android-demo/app/src/main/java/com/lhc/ocat/android_demo/WebActivity.java
@@ -0,0 +1,35 @@
+package com.lhc.ocat.android_demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        mWebView = findViewById(R.id.webView);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+            }
+        });
+    }
+}
