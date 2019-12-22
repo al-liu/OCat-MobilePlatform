d480c03ca9b4dce18b9dec5efcf6eb69ff7611b9
刘海川 <lhc.indie@gmail.com>
2019-12-22 18:08:01 +0800

    Update vue.config.js

--- mobileplatform-portal/vue.config.js
+++ mobileplatform-portal/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     port: 8082,
     proxy: {
       '/api': {
-        target: 'http://localhost:8800',
+        target: 'http://localhost:9090',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
